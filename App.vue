8649583b4cabca7196ce5be8bdc87d8888e14f07
Ken Berkeley <kenberkeley@foxmail.com>
2018-01-10 15:55:03 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,48 @@
+<template>
+  <tree-node :data="treeData" />
+</template>
+<script>
+import TreeNode from './TreeNode'
+
+export default {
+  components: { TreeNode },
+  data: () => ({
+    treeData: {
+      label: 'Company',
+      children: [
+        { label: 'Ken',
+          children: [
+            { label: 'Ben' },
+            { label: 'Karen',
+              children: [
+                { label: 'Amy' }
+              ]
+            },
+            { label: 'John' },
+            { label: 'Mike',
+              children: [
+                { label: 'Mary',
+                  children: [
+                    { label: 'James',
+                      children: [
+                        { label: 'Susan' }
+                      ]
+                    }
+                  ]
+                },
+                { label: 'Lisa' }
+              ]
+            }
+          ]
+        },
+        {
+          label: 'Jason',
+          children: [
+            { label: 'Rose' }
+          ]
+        }
+      ]
+    }
+  })
+}
+</script>
